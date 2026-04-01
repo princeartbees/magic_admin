485015df6e7cb92edfba375d461ed31a52b00fda
princeartbees <126044337+princeartbees@users.noreply.github.com>
2026-04-01 15:58:28 +0530

    update version

--- dist/MHC_Angular/server/main.server.mjs
+++ dist/MHC_Angular/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QTPNI4MC.mjs";import"./chunk-2XS5IYYN.mjs";import"./chunk-IJNRGZNA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-F7QZFPUZ.mjs";import"./chunk-VXRJIDDL.mjs";import"./chunk-3777ELRS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
